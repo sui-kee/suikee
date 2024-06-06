5d3c3ab7a26f3b14452890fb1465244eb24ba57d
hyenaManager <zodiacjelly9@gmail.com>
2024-06-06 19:55:11 +0800

    modify: animation in provides

--- src/components/myProvides.jsx
+++ src/components/myProvides.jsx
@@ -36,41 +36,42 @@ export default function MyProvides() {
         ))}
       </motion.div>
       {provides.map((provide, index) => (
-        <Provide key={index} provide={provide} />
+        <Provide key={index} provide={provide} timeIndex={index} />
       ))}
     </section>
   );
 }
 
-const Provide = ({ provide }) => {
+const Provide = ({ provide, timeIndex }) => {
   const ref = useRef(null);
   const animation = useAnimation();
   const isInView = useInView(ref);
   useEffect(() => {
     if (isInView) {
-      animation.start({ scale: [0.2, 0.3, 0.4, 0.6, 0.8, 0.9, 1] });
+      animation.start({ x: 0 });
     } else {
-      animation.start({ scale: 0.1 });
+      animation.start({ x: "100vw" });
     }
   }, [isInView]);
   return (
-    <motion.article
-      initial={{ scale: 0.1 }}
-      animate={animation}
-      transition={{ duration: 3, type: "spring" }}
-      ref={ref}
-      className=" flex justify-start flex-col w-[250px] sm:w-[300px] p-3 items-center rounded-tl-3xl rounded-br-3xl bg-slate-200"
-    >
-      <img
-        src={provide.image}
-        width={180}
-        height={180}
-        alt="provides"
-        className=" w-[150px] h-[150px]"
-      />
-      <h3 className="text-black text-sm sm:text-lg lg:text-2xl">
-        {provide.name}
-      </h3>
-    </motion.article>
+    <motion.div ref={ref}>
+      <motion.article
+        initial={{ x: "100vw" }}
+        animate={animation}
+        transition={{ duration: 3, type: "spring", delay: timeIndex / 8 }}
+        className=" flex justify-start flex-col w-[250px] sm:w-[300px] p-3 items-center rounded-tl-3xl rounded-br-3xl bg-slate-200"
+      >
+        <img
+          src={provide.image}
+          width={180}
+          height={180}
+          alt="provides"
+          className=" w-[150px] h-[150px]"
+        />
+        <h3 className="text-black text-sm sm:text-lg lg:text-2xl">
+          {provide.name}
+        </h3>
+      </motion.article>
+    </motion.div>
   );
 };
